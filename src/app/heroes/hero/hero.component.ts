cec1d7e52686649fd2f1b222517ec197b888c6d6
Luis Panameno <luispanamenov54@gmail.com>
2023-12-17 14:04:54 -0600

    Fin de sección 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css',
+})
+export class HeroComponent {
+  public name: string = 'Ironman';
+  public age: number = 45;
+
+  get capitalizedName(): string {
+    return 'Hola mundo';
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeName(): void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 20;
+  }
+  reset(): void {
+    this.name = 'Ironman';
+    this.age = 45;
+  }
+}
